53e4fdbdbe86f8033309492c62a23245d553d62d
Adrian <adrianhyk@gmail.com>
2017-11-28 23:58:39 +0800

    add user login panel to the navibar - no functionality yet, just a placeholder

--- components/Navibar.vue
+++ components/Navibar.vue
@@ -1,35 +1,43 @@
 <template>
    <nav class="navbar is-fixed-top" role="navigation" aria-label="main navigation">
-      <div class="container">
-         <div class="navbar-brand">
-            <div class="navbar-item">
-               <h1 class="has-text-weight-bold is-size-4">The Library</h1>
-            </div>
-            
-            <!-- navbar items, navbar burger... -->
-         </div>
+	<div class="container">
+	   <div class="navbar-brand">
+		<div class="navbar-item">
+		   <logo></logo>
+		</div>
+	   </div>
 
-         <div class="navbar-menu">
-            <div class="navbar-start">
-                  <router-link to="/" class="navbar-item">
-                        Home
-                  </router-link>
-                  <router-link to="/stories" class="navbar-item">
-                        Stories
-                  </router-link>
-            </div>
+	   <div class="navbar-menu">
+		<div class="navbar-start">
+		   <router-link to="/" class="navbar-item">
+			Home
+		   </router-link>
+		   <router-link to="/stories" class="navbar-item">
+			Stories
+		   </router-link>
+		</div>
 
-            <div class="navbar-end">
-            <!-- navbar items -->
-            </div>
-         </div>
-      </div>      
+		<div class="navbar-end">
+		   <user-login-panel></user-login-panel>
+		</div>
+	   </div>
+	</div>      
    </nav>
 </template>
 
 <script>
+import UserLoginPanel from "~/components/UserLoginPanel"
+import Logo from "~/components/Logo"
+
 export default {
-   name : "navibar"
+   name : "navibar",
+   components :{UserLoginPanel, Logo},
+   data(){
+	return {}
+   },
+   computed(){
+	return {}
+   }
 }
 </script>
 
---
+++ components/UserLoginPanel.vue
@@ -0,0 +1,31 @@
+<template>
+   <div class="navbar-item">
+      <template v-if="isLoggedIn">
+			<a href="#" class="navbar-item">
+			   Logout
+			</a>
+		   </template>
+		   <template v-else>
+			<a href="#" class="navbar-item">
+			   Login
+			</a>
+			<a href="#" class="navbar-item">
+			   Register
+			</a>
+      </template>
+   </div>
+</template>
+
+<script>
+export default {
+   name:"user-login-panel",
+   computed(){
+	   return {
+         isLoggedIn(){
+            return false;
+         }
+	   }
+   }
+}
+</script>
+
