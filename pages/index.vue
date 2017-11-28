2060165a5f266914b6c9a2064a2e7f926f2d1680
Adrian <adrianhyk@gmail.com>
2017-11-28 23:35:28 +0800

    port to Nuxt.js for easy SSR

---
+++ pages/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <section class="">
+      <h1 class="title">Placeholder for Index Page</h1>
+  </section>
+</template>
+
+<script>
+export default {
+  components: {}
+}
+</script>
+
+<style>
+</style>
---
+++ pages/stories.vue
@@ -0,0 +1,10 @@
+<template>
+  <h1 class="title">Placeholder for Stories</h1>
+</template>
+
+<script>
+export default {
+  
+}
+</script>
+
