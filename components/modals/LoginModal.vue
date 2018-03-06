1dd28ffd2d6b3dcc50f9e28fe421172bbbc6e66b
Adrian <adrianhyk@gmail.com>
2018-03-06 23:52:24 +0800

    make login modal a bit more visually presentable

--- components/modals/LoginModal.vue
+++ components/modals/LoginModal.vue
@@ -1,18 +1,36 @@
 <template>
    <div class="modal-card">
       <header class="modal-card-head">
-         <p class="modal-card-title">Modal title</p>
+         <p class="modal-card-title">Login</p>
          <button class="delete" aria-label="close" @click="close"></button>
       </header>
       <section class="modal-card-body">
-         <div>
-            <input type="text" name="username" v-model="username" />
-            <br>
-            <input type="password" name="password" v-model="password" />
-            <button @click="login">
-               Login
-            </button>
+         <div class="field">
+            <label class="label"> Username </label>
+            <div class="control">
+              <input 
+                type="text" name="username" 
+                v-model="username" 
+                class="input" 
+              />
+            </div>           
          </div>
+        <div class="field">
+            <label class="label"> Password </label>
+            <div class="control">
+              <input 
+                type="password" name="password" 
+                v-model="password"
+                class="input"  
+              />
+            </div>
+        </div>
+        <button 
+          @click="login"
+          class="button is-primary pull-right"
+        >
+            Login
+        </button>
       </section>
    </div>
 </template>
