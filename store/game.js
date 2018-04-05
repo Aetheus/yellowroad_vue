d95a8657e183f82d970ebe41704e104e4995bb54
Adrian <adrianhyk@gmail.com>
2018-04-06 01:56:43 +0800

    add game store

---
+++ store/game.js
@@ -0,0 +1,58 @@
+export const state = () => ({
+   stories : {
+      /*
+         [story_id] : {
+            states : [
+               {save : {}, current_chapter_id : 1, chapter_path_id: null},
+               {save : {job:"knight"}, current_chapter_id : 2, chapter_path_id: 1},
+            ]
+            cursor : 1
+         }
+      */
+   }
+})
+
+export const mutations = {
+   ensureStory(store, story_id){
+      if (!store.stories[story_id]){
+         store.stories[story_id] = {
+            states : [
+               //{save : {}, current_chapter_id : 2, chapter_path_id: 1},
+            ],
+            cursor: 0
+         }
+      }
+   },
+   pushNewState(store, { story_id, chapter_id, chapter_path_id, save }){
+      let cursor = store.stories[story_id].cursor;
+      let branch = store.stories[story_id].states.slice(0,cursor+1);
+
+      branch.push({ 
+         save, 
+         chapter_path_id, 
+         current_chapter_id: chapter_id 
+      })
+      
+      store.stories[story_id].states = branch;
+   },
+   travelState(store, {cursor}){
+      
+   }
+}
+
+export const actions = {
+   advanceAlongPath(
+      {commit, dispatch}, 
+      {story_id, chapter_id, chapter_path_id, current_save}
+   ){
+      commit("ensureStory", story_id);
+
+      let result = await this.$axios.post(`/api/stories/${story_id}/chapters/${chapter_id}/game`,{
+         save: current_save,
+         chapter_path_id
+      })      
+
+      commit("pushNewState", { story_id, chapter_id, chapter_path_id, save : current_save})
+      commit("chapters/setChapter", result.data.chapter)
+   }
+}
