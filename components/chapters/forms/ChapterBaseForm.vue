95930a42522579e56e817ab591dbf066d4ce5480
Adrian <adrianhyk@gmail.com>
2018-05-20 01:20:15 +0800

    - ensure that form settings will always be able to be updated via props by making "settings" a computed property

--- components/chapters/forms/ChapterBaseForm.vue
+++ components/chapters/forms/ChapterBaseForm.vue
@@ -1,69 +1,62 @@
 <template>
-   <div>
-      <div v-if="header" class="field">
-         <h1 class="title">
-            {{header}}
-         </h1>
-      </div>
+	<div>
+		<div v-if="header" class="field">
+			<h1 class="title">
+				{{header}}
+			</h1>
+		</div>
 
-      <div class="field">
-         <label class="label" for="chapter-title">Title</label>
-         <div class="control">
-            <input name="chapter-title" class="input" type="text" placeholder="Chapter Title" v-model="form_state.title">
-         </div>
-      </div>
-      <div class="field">
-         <label class="label" for="chapter-body">Body</label>
-         <div class="control">
-            <!--TODO: replace with a proper Markdown editor with a preview, buttons for common styles, etc -->
-            <textarea 
-               name="chapter-body" 
-               class="textarea" 
-               :rows="settings.initial_body_rows"
-               v-model="form_state.body"
-               placeholder="It all began with ... ">
-            </textarea>
-         </div>
-      </div>
-      <slot></slot>
-      <div class="field is-grouped">
-         <p class="control">
-            <a v-on:click="this.emitSubmit" class="button is-primary"> Submit </a>
-         </p>
-         <p class="control">
-            <a v-on:click="this.emitCancel" class="button is-light"> Cancel </a>
-         </p>
-      </div>
-   </div>
+		<div class="field">
+			<label class="label" for="chapter-title">Title</label>
+			<div class="control">
+				<input name="chapter-title" class="input" type="text" placeholder="Chapter Title" v-model="form_state.title">
+			</div>
+		</div>
+		<div class="field">
+			<label class="label" for="chapter-body">Body</label>
+			<div class="control">
+				<!--TODO: replace with a proper Markdown editor with a preview, buttons for common styles, etc -->
+				<textarea name="chapter-body" class="textarea" :rows="settings.initial_body_rows" v-model="form_state.body" placeholder="It all began with ... ">
+				</textarea>
+			</div>
+		</div>
+		<slot></slot>
+		<div class="field is-grouped">
+			<p class="control">
+				<a v-on:click="this.emitSubmit" class="button is-primary"> Submit </a>
+			</p>
+			<p class="control">
+				<a v-on:click="this.emitCancel" class="button is-light"> Cancel </a>
+			</p>
+		</div>
+	</div>
 </template>
 
 <script>
 export default {
-   name : "chapter-base-form",
-   components :{},
-   props : ["initialFormState", "formSettings", "header", "chapterId", "storyId"],
-   data(){
+   name: "chapter-base-form",
+   components: {},
+   props: ["initialFormState", "formSettings", "header", "chapterId", "storyId"],
+   data() {
+		const defaultFormState = {title:"",body:""}
+
       return {
-         settings : Object.assign({
-            initial_body_rows : 18
-         },this.formSettings),
-         form_state : (
-            this.initialFormState ? this.initialFormState : {
-                  title : "",
-                  body  : "",
-            }
-         )
+			form_state: { ...defaultFormState , ...this.initialFormState,  }
       }
-   },
-   methods:{
-      emitSubmit(){
+	},
+	computed: {
+		settings(){
+			const defaultSettings = {initial_body_rows: 18}
+			return { ...defaultSettings , ...this.formSettings };
+		}
+	},
+   methods: {
+      emitSubmit() {
          this.$emit("submit", this.form_state)
       },
-      emitCancel(){
+      emitCancel() {
          this.$emit("cancel", this.form_state)
       }
-   },
-   computed: {
    }
 }
 </script>
