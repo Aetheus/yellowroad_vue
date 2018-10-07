96f076c938d40b8cebc790d9a867faa97eb22ef9
Adrian <adrianhyk@gmail.com>
2018-10-07 22:01:02 +0800

    upgrade to current version of Nuxt.js

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,53 +1,73 @@
+const pkg = require('./package')
+
 module.exports = {
+  mode: 'universal',
+
   /*
   ** Headers of the page
   */
   head: {
-    title: 'yellowroad_vue',
+    title: pkg.name,
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: 'Nuxt.js project' }
+      { hid: 'description', name: 'description', content: pkg.description }
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
     ]
   },
-  router: {
-    middleware: "auth_cookie_to_store"
-  },
-  modules: [
-    '@nuxtjs/axios',
+
+  /*
+  ** Customize the progress-bar color
+  */
+  loading: { color: '#fff' },
+
+  /*
+  ** Global CSS
+  */
+  css: [
   ],
+
+  /*
+  ** Plugins to load before mounting the App
+  */
   plugins: [
     { src: '~/plugins/client_side_token_auth', ssr: false }
   ],
+
+  /** Middleware for the routes */
+  router: {
+    middleware: "auth_cookie_to_store"
+  },
+    
+  /*
+  ** Nuxt.js modules
+  */
+  modules: [
+    // Doc: https://github.com/nuxt-community/axios-module#usage
+    '@nuxtjs/axios'
+  ],
+  /*
+  ** Axios module configuration
+  */
   axios: {
+    // See https://github.com/nuxt-community/axios-module#options
     baseURL: "http://localhost:8080/api",
     browserBaseURL: "http://localhost:8080/api",
     debug: true,
     // proxyHeaders: false
   },
-  /*
-  ** Customize the progress bar color
-  */
-  loading: { color: '#3B8070' },
+
   /*
   ** Build configuration
   */
-  // build: {
-  //   /*
-  //   ** Run ESLint on save
-  //   */
-  //   extend (config, ctx) {
-  //     if (ctx.dev && ctx.isClient) {
-  //       config.module.rules.push({
-  //         enforce: 'pre',
-  //         test: /\.(js|vue)$/,
-  //         loader: 'eslint-loader',
-  //         exclude: /(node_modules)/
-  //       })
-  //     }
-  //   }
-  // }
+  build: {
+    /*
+    ** You can extend webpack config here
+    */
+    extend(config, ctx) {
+      
+    }
+  }
 }
